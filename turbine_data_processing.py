ffd5526ee25d773225b33e77775db295a5b1d456
marcobalbontin <marcobalbontinponce@gmail.com>
2026-06-17 12:07:43 +0100

    feat: print all anomalies found

--- turbine_data_processing.py
+++ turbine_data_processing.py
@@ -6,3 +6,6 @@ temp_anomaly = df['temperature_c'] > 85.0 # variable defined for for temperature
 vibration_anomaly = df['vibration_mm_s'] > 15.0 # variable defined for vibration anomalies
 
 anomalies = df[temp_anomaly | vibration_anomaly]
+
+print(f"Found {len(anomalies)} anomalies out of {len(df)} records")
+print(anomalies)
