fce5d67fb19e2a63e990b3accc8a4a16f0d1cc84
marcobalbontin <marcobalbontinponce@gmail.com>
2026-06-17 12:18:01 +0100

    chore: add detailed comments

--- turbine_data_processing.py
+++ turbine_data_processing.py
@@ -2,10 +2,10 @@ import pandas as pd
 
 df = pd.read_csv('telemetry_data.csv') # loads csv file and stores in a dataframe
 
-temp_anomaly = df['temperature_c'] > 85.0 # variable defined for for temperature anomalies
-vibration_anomaly = df['vibration_mm_s'] > 15.0 # variable defined for vibration anomalies
+temp_anomaly = df['temperature_c'] > 85.0 # variable holding temperature anomalies (true/false)
+vibration_anomaly = df['vibration_mm_s'] > 15.0 # variable holding vibration anomalies (true/false)
 
-anomalies = df[temp_anomaly | vibration_anomaly]
+anomalies = df[temp_anomaly | vibration_anomaly] # variable holding all anomalies found (either temperature or vibration anomalies) 
 
-print(f"Found {len(anomalies)} anomalies out of {len(df)} records")
-print(anomalies)
+print(f"Found {len(anomalies)} anomalies out of {len(df)} records") # string printed using f-strings with total number of anomalies
+print(anomalies) # prints the anomalies dataframe
