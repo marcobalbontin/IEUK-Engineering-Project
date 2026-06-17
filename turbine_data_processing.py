76063518c8b0553d3c97e9abfbb92776e596f37c
marcobalbontin <marcobalbontinponce@gmail.com>
2026-06-17 12:07:14 +0100

    feat: define anomaly conditions via variables

--- turbine_data_processing.py
+++ turbine_data_processing.py
@@ -1,3 +1,8 @@
 import pandas as pd
 
 df = pd.read_csv('telemetry_data.csv') # loads csv file and stores in a dataframe
+
+temp_anomaly = df['temperature_c'] > 85.0 # variable defined for for temperature anomalies
+vibration_anomaly = df['vibration_mm_s'] > 15.0 # variable defined for vibration anomalies
+
+anomalies = df[temp_anomaly | vibration_anomaly]
